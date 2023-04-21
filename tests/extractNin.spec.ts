e72440f9d279371f38b8dd95315107c0405da3de
Michal Valeš <michal.vales@xcd.cz>
2023-04-21 18:52:13 +0200

    extractNin tests

--- tests/extractNin.spec.ts
+++ tests/extractNin.spec.ts
@@ -1,8 +1,10 @@
 import extractNin from '../src/extractNin'
 import {
   generalValid,
+  generalInvalid,
   inputFormats,
   lengthThresholds,
+  overflowThresholds,
   NinTestItem,
 } from './cases/nin'
 
@@ -23,5 +25,7 @@ const applyTestCases = (items: NinTestItem[]): void => items.forEach(({ descript
 ))
 
 describe('general valid NINs', () => applyTestCases(generalValid))
+describe('general invalid NINs', () => applyTestCases(generalInvalid))
 describe('input format', () => applyTestCases(inputFormats))
 describe('length threshold', () => applyTestCases(lengthThresholds))
+describe('overflow threshold', () => applyTestCases(overflowThresholds))
